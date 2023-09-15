0c5ecd7e496b182ae883f01926068c1967bfab48
humy06001 <humy06001@E-PW0234KS>
2023-09-15 13:49:32 +0200

    Add project files.

---
+++ Kap 2 Uppgift 2.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Skriv en mening
+            Console.WriteLine("Skriv en mening med tre ord och en punkt på slutet.");
+            string sentance = Console.ReadLine();
+
+            //Indexen för mellanslagen samt punkt
+            int space1Index = sentance.IndexOf(" ");
+            int space2Index = sentance.IndexOf(" ", (space1Index+1));
+            int periodIndex = sentance.IndexOf(".");
+
+            //string för varje ord
+            string word1 = sentance[..space1Index];
+            string word2 = sentance[(space1Index+1)..space2Index];
+            string word3 = sentance[(space2Index+1)..periodIndex];
+
+            //Skriver ut meningen baklänges
+            Console.WriteLine($"Din mening baklänges: {word3} {word2} {word1}.");
+            Console.ReadKey();
+        }
+    }
+}
